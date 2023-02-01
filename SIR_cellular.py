144ac2462ea5ab0e4cda1d5fc52c48f68aadd5d7
tomhruby1 <tom.hruby1@gmail.com>
2023-02-01 15:58:32 +0100

    fixing to [0,1]

--- SIR_cellular.py
+++ SIR_cellular.py
@@ -1,10 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 from pathlib import Path
+
+
+def init_state(N_pop, V, virulence, gamma, connection, movement, infected_coords, infected_count):
+    dim = N_pop.shape
+    mu = np.ones((dim[0], dim[1], len(V))) * connection * movement * virulence
+    
+    S = np.ones((dim[0], dim[1], 3)) #state consisting of SIR values
+    S[:,:,0] *= 1
+    S[:,:,1] *= 0
+    S[:,:,2] *= 0
+    S[infected_coords[0], infected_coords[1], 1] = infected_count / N_pop[infected_coords] 
+    S[infected_coords[0], infected_coords[1], 0] = 1 - infected_count / N_pop[infected_coords] 
+
+    print(f"Init spread with {infected_count} infected individuals ({S[infected_coords[0], infected_coords[1], 1] * 100}% local pop.) at cell {infected_coords}.")
+
+    return S, mu
 
 def plot_cell_state(state, colormap=None, title=None, save_path=None, show=True):
     fig, ax = plt.subplots()
-    p = ax.pcolormesh(S[:,:,1], cmap=colormap, rasterized=True, vmin=0, vmax=5)
+
+    p = ax.pcolormesh(state, cmap=colormap, rasterized=True, vmin=0, vmax=1)
     fig.colorbar(p, ax=ax)
     if title is not None: plt.title(title)
     if save_path is not None:
@@ -12,10 +30,14 @@ def plot_cell_state(state, colormap=None, title=None, save_path=None, show=True)
     if show: plt.show()
 
 def time_step(S, beta, gamma, mu, N, V):
+    dim = (S.shape[0], S.shape[1])
+    #TODO: remove deepcopy
+    S_past = copy.deepcopy(S[:,:,0])
+    I_past = copy.deepcopy(S[:,:,1])
     for i in range(dim[0]):
         for j in range(dim[1]):
-            S_past = S[i,j][0]
-            I_past = S[i,j][1]
+            if N[i,j] < 1.0: #skip if population zero 
+                continue
             sum = 0
             # calculate the connection sum
             for v in V:
@@ -23,14 +45,15 @@ def time_step(S, beta, gamma, mu, N, V):
                 ij = np.asarray([i,j])
                 # considering only cells in the grid TODO: maybe add padding around?
                 if (ij + v >= 0).all() and ((ij + v)[0] < dim[0]) and ((ij + v)[1] < dim[1]): 
-                    sum += N[tuple(ij + v)] * mu[i,j,V.index(tuple(v))] * S[tuple(ij+v)][1] 
+                    sum += N[tuple(ij + v)]/N[tuple(ij)] * mu[i,j,V.index(tuple(v))] * I_past[tuple(ij+v)] 
             #   S
-            S[i,j][0] += -beta * S_past * I_past - S_past * sum
+            S[i,j][0] += -beta * S_past[i,j] * I_past[i,j] - S_past[i,j] * sum
             #   I
-            S[i,j][1] = (1-gamma)*I_past + beta * S_past * I_past + S_past * sum
+            S[i,j][1] = (1-gamma) * I_past[i,j] + beta * S_past[i,j] * I_past[i,j] + S_past[i,j] * sum
             #   R
-            S[i,j][2] += gamma * I_past
+            S[i,j][2] += gamma * I_past[i,j]
     return S
+
 
 if __name__=='__main__':
     #sim setup
@@ -47,7 +70,7 @@ if __name__=='__main__':
     out_p.mkdir(exist_ok=True)
     
     N = np.ones(dim) * 5 # --> calc automat. from initial SIR vals for each cell
-    mu_dim = (dim[0], dim[1], 8)    #mu param for each neighbor for each cell
+    mu_dim = (dim[0], dim[1], len(V))    #mu param for each neighbor for each cell
     mu = np.ones(mu_dim) * 0.005
 
     S = np.ones((dim[0], dim[1], 3)) #state consisting of SIR values
@@ -65,7 +88,5 @@ if __name__=='__main__':
     # RUN SIM
     for t in range(T):
         S = time_step(S, beta, gamma, mu, N, V)
-        
-        save_name = 'step_'+(3-len(str(t)))*'0'+str(t)+'.png'
         plot_cell_state(S[:,:,1],  colormap='viridis', title=f"Infected t={t}", 
-                        save_path=str(out_p/save_name), show=False)
+                        save_path=str(out_p/f'step_{t}.png'), show=False)
